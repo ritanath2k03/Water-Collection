a3f31c2a2f85741733fceaad888f97ef54a6733f
Ritanath Malakar <ritanath2003@gmail.com>
2025-06-21 11:32:22 +0530

    multiple nav added in main activity

--- app/src/main/java/com/rith/muski/MainActivity.kt
+++ app/src/main/java/com/rith/muski/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.rith.muski
 
 import android.os.Bundle
+import android.view.View
 import android.view.Window
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -26,7 +27,71 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        setNavs()
+    }
 
+    private fun setNavs() {
+        val genericNavHost = supportFragmentManager.findFragmentById(R.id.fragmentContainerView2) as NavHostFragment
+        val bottomNavHost = supportFragmentManager.findFragmentById(R.id.bottom_nav_host) as NavHostFragment
+
+        secondaryNavController = genericNavHost.navController
+        bottomNavController = bottomNavHost.navController
+
+        val bottomNavView = findViewById<BottomNavigationView>(R.id.bottom_nav_view)
+
+        bottomNavView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.dashboard -> {
+                    showGenericHost()
+                    secondaryNavController.navigate(R.id.dashboardFragment)
+                    true
+                }
+
+                R.id.userList -> {
+                    showBottomHost()
+                    bottomNavController.navigate(R.id.userListFragment2)
+                    true
+                }
+
+                R.id.profile -> {
+                    showBottomHost()
+                    bottomNavController.navigate(R.id.profileFragment2)
+                    true
+                }
+
+                else -> false
+            }
+
+        }
+    }
+    private fun showGenericHost() {
+        findViewById<View>(R.id.fragmentContainerView2).visibility = View.VISIBLE
+        findViewById<View>(R.id.bottom_nav_host).visibility = View.GONE
+    }
+
+    private fun showBottomHost() {
+        findViewById<View>(R.id.fragmentContainerView2).visibility = View.GONE
+        findViewById<View>(R.id.bottom_nav_host).visibility = View.VISIBLE
+    }
+
+
+    override fun onBackPressed() {
+        val isGenericVisible = findViewById<View>(R.id.fragmentContainerView2).visibility == View.VISIBLE
+
+        if (isGenericVisible) {
+            val currentDestination = secondaryNavController.currentDestination?.id
+
+            if (currentDestination == R.id.dashboardFragment) {
+                // 🔥 Exit if on dashboard
+                finish()
+            } else {
+                // ⬅️ Navigate back in generic stack
+                if (!secondaryNavController.popBackStack()) finish()
+            }
+        } else {
+            // ❌ No backstack for bottom nav → Exit
+            super.onBackPressed()
+        }
     }
 
 }
